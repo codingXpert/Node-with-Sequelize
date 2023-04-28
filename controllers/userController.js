fceedb21aad7043faf1af6d312b7b068e98190c1
Vivek <vivekraj78965.vr@gmail.com>
2023-04-28 21:58:09 +0530

    updating an user using update()

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,14 @@
 const db = require('../models');
 const User = db.user;
 var addUser = async(req , res) => {
-    // const jane = User.build({ firstName: "Jane" , lastName:'kumar'});
+
      const jane = await User.create({ firstName: "Jane" , lastName:'singh'});
-    console.log(jane instanceof User); // true
-    console.log(jane.firstName); // "Jane"
-    // await jane.save();
+    console.log(jane instanceof User); 
+    console.log(jane.firstName); 
+
 
     jane.set({firstName: "Sohan" , lastName:'singh'});
+    await jane.update({ firstName: "Mohit" , lastName:'singh' })
     await jane.save() // in case of update we can use save() with update()
     res.status(200).json(jane.toJSON());
 }
