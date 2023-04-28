0f7dbbf9c14e681d27c2f8b8722d532bd68ef888
Vivek <vivekraj78965.vr@gmail.com>
2023-04-28 21:38:37 +0530

    inserting data in db using create()

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,13 @@
 const db = require('../models');
 const User = db.user;
 var addUser = async(req , res) => {
-    const jane = User.build({ firstName: "Jane" , lastName:'kumar'});
+    // const jane = User.build({ firstName: "Jane" , lastName:'kumar'});
+    const jane = User.create({ firstName: "Jane" , lastName:'singh'});
     console.log(jane instanceof User); // true
-    console.log(jane.name); // "Jane"
-    await jane.save();
+    console.log(jane.firstName); // "Jane"
+    // await jane.save();
     console.log('Jane was saved to the database!');
-    res.status(200).json(jane.toJSON());
+    // res.status(200).json(jane.toJSON());
 }
 
 module.exports = {
